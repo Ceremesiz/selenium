72a179d7676a489ea252c34994a95557b4b4059c
Taner_Ceremesiz <taner.ceremesiz@testconsulting.de>
2025-07-19 17:01:33 +0200

    [MISC] new class

--- src/test/java/Test/Crmz.java
+++ src/test/java/Test/Crmz.java
@@ -1,24 +1,24 @@
 package Test;
 
-import General.BaseMethoden;
+import BaseMethoden.BaseMethoden;
+import Pageinformation.Homepage;
+import Pageinformation.Locator;
+import dataProvider.DataProviderDoD;
 import org.testng.Assert;
-import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
 public class Crmz extends BaseMethoden {
 
 
-    @Test(groups = "smoketest")
-    public void LoginTestPositive2(){
+    @Test(dataProvider = "credentials", dataProviderClass = DataProviderDoD.class)
+    public void LoginTestPositive2(String username, String password){
         //Homepage login;
-        homepage.enterLogin("student","Password123");
+        homepage.enterLogin(username,password);
         // homepage.UrlPruefung();
-        Assert.assertTrue(homepage.getPageResult(LOGIN));
+        Assert.assertTrue(homepage.getPageResult(LOGIN)); // LOGIN weist auf final Variable hin
         homepage.logoutClick();
         Assert.assertTrue(homepage.getPageResult("Logout"));
 
         //homepage.logout();
     }
-
-
 }
